678d15daca660e7014ec53b1f74316d10dce5bc5
John Ryan <uijedi@gmail.com>
2018-12-05 22:35:56 -0500

    passing needs speed

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -27,6 +27,7 @@ def TF_Matrix(alpha, a, d, q):
             [                      0,                 0,           0,             1]])
     return TF
 
+### Define functions for homogeneous transform matrices of rotations about x, y, z.
 def Rot_x(q):	
     R_x = Matrix([[ 1,              0,        0],
                 [ 0,        cos(q), -sin(q)],
@@ -63,13 +64,13 @@ def handle_calculate_IK(req):
         r, p, y = symbols('r p y')
 
 	    # Create Modified DH parameters
-        s = {alpha0:    0,  a0:      0,  d1:  0.75,   q1:           q1,
-            alpha1: -pi/2.,  a1:   0.35,  d2:     0,  q2:  -pi/2. + q2,
-            alpha2:     0,   a2:   1.25,  d3:     0,   q3:           q3,
+        s = {alpha0:        0,  a0:      0,  d1:  0.75,  q1:           q1,
+            alpha1: -pi/2.,  a1:   0.35,  d2:     0,  q2:  q2-pi/2.,
+            alpha2:         0,  a2:   1.25,  d3:     0,  q3:           q3,
             alpha3: -pi/2.,  a3: -0.054,  d4:  1.50,  q4:           q4,
             alpha4:  pi/2.,  a4:      0,  d5:     0,  q5:           q5,
             alpha5: -pi/2.,  a5:      0,  d6:     0,  q6:           q6,
-            alpha6:     0,  a6:      0,  d7: 0.303,   q7:            0}
+            alpha6:         0,  a6:      0,  d7: 0.303,  q7:            0}
 
 	
 	    # Create individual transformation matrices
@@ -79,10 +80,10 @@ def handle_calculate_IK(req):
         T3_4 = TF_Matrix(alpha3, a3, d4, q4).subs(s)
         T4_5 = TF_Matrix(alpha4, a4, d5, q5).subs(s)
         T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(s)
-        T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(s)
+        T6_G = TF_Matrix(alpha6, a6, d7, q7).subs(s)
 
         # Extract rotation matrices from the transformation matrices
-        T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+        T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
 
         # Correcting difference in orientation between DH convention and URDF file
         R_x = Rot_x(r)
@@ -90,6 +91,7 @@ def handle_calculate_IK(req):
         R_z = Rot_z(y)
 
 
+        # Initialize service response
         for x in xrange(0, len(req.poses)):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
@@ -107,27 +109,22 @@ def handle_calculate_IK(req):
 
             ### Your IK code here
             # Compensate for rotation discrepancy between DH parameters and Gazebo
-
-            # http://planning.cs.uiuc.edu/node102.html
-
             ROT_EE = R_z * R_y * R_x
-            # see KR120 fk
             Rot_Error = R_z.subs(y, radians(180)) * R_y.subs(p, radians(-90))
-
             ROT_EE = ROT_EE * Rot_Error
             ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
 
-
+            # Calculate joint angles using Geometric IK method
             EE = Matrix([[px],
-                        [py],
-                        [pz]])
+                         [py],
+                         [pz]])
 
+            # Calculate the wrist center position
             WC = EE - (0.303) * ROT_EE[:,2]
-
 
             theta1 = atan2(WC[1], WC[0])
                     
-            # SS triangle theta2 and theta3
+            # SSS triangle  to find theta2 and theta3
             side_a = 1.501
             side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
             side_c = 1.25
@@ -137,9 +134,10 @@ def handle_calculate_IK(req):
             angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
             
                         
-            theta2 = np.pi / 2. - angle_a - atan2(WC[2] - 0.75,  sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
-            theta3 = np.pi / 2. - (angle_b + 0.036) # 0.036 accounts for sag in link4 of 0.054m
+            theta2 = np.pi / 2 - angle_a - atan2(WC[2] - 0.75,  sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
+            theta3 = np.pi / 2 - (angle_b + 0.036) # 0.036 accounts for sag in link4 of 0.054m
 
+            # Calculate Euler angles from orientation
             R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
             R0_3 = R0_3.evalf(subs={'q1': theta1, 'q2': theta2, 'q3': theta3})
 
@@ -147,27 +145,17 @@ def handle_calculate_IK(req):
             # using transpose instead:
             R3_6 = R0_3.transpose() * ROT_EE
 
-
-            # Calculate joint angles using Geometric IK method
-            # Euler angles from rotation matrix
-            
             theta5 =  atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2]), R3_6[1,2])
 
-        
-            # theta5 = atan2(sqrt(R3_6[0,2]**2 + R3_6[2,2]**2), R3_6[1,2])
-            # Choosing between multiple possible solutions:
+            # There are 2 possible solutions for theta5. 
+            # Depending on theta5 value choose theat4 and theta6
             if theta5 > pi:
                 theta4 = atan2(-R3_6[2,2], R3_6[0,2])
                 theta6 = atan2(R3_6[1,1], -R3_6[1,0])
             else:
                 theta4 = atan2(R3_6[2,2], -R3_6[0,2])
                 theta6 = atan2(-R3_6[1,1], R3_6[1,0])
-
-
-            # theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-            # theta6 = atan2(-R3_6[1,1], R3_6[1,0])
-            #https://github.com/mkhuthir/RoboND-Kinematics-Project/blob/master/src/kuka_arm/scripts/IK_server.py
-
+                
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
             joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
